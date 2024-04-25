db1e78fb19363716cee2f688be9ba1796d683aa1
Kailash Nath <kailashnathashok@gmail.com>
2024-04-25 06:35:48 +0300

    setup the canvas

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from tkinter import Tk, Canvas, PhotoImage
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+# window
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+# canvas
+
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+window.mainloop()
