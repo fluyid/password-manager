e39cb9378a38ba8a2eb50e1a3105e4cc50153379
Kailash Nath <kailashnathashok@gmail.com>
2024-04-25 08:34:50 +0300

    gave the add button a functionality

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button
+from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button, END
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    """used to save data to a text file"""
+    site = website_input.get()
+    login = login_id_input.get()
+    passkey = password_input.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{site} | {login} | {passkey}\n")
+        website_input.delete(0, END)
+        password_input.delete(0, END)
+        website_input.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -33,9 +46,11 @@ password.grid(row=3, column=0)
 
 website_input = Entry()
 website_input.grid(row=1, column=1, columnspan=2, sticky="ew")
+website_input.focus()
 
 login_id_input = Entry()
 login_id_input.grid(row=2, column=1, columnspan=2, sticky="ew")
+login_id_input.insert(0, "kailash@gmail.com")
 
 password_input = Entry()
 password_input.grid(row=3, column=1, sticky="ew")
@@ -45,7 +60,7 @@ password_input.grid(row=3, column=1, sticky="ew")
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2, sticky="ew")
 
-add_to_directory_button = Button(text="Add", width=36)
+add_to_directory_button = Button(text="Add", width=36, command=save)
 add_to_directory_button.grid(row=4, column=1, columnspan=2, sticky="ew")
 
 # Window exit
